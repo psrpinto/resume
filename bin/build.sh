924df558c8da9a0969429654f84ea7d3309f745d
Paulo Rodrigues Pinto <regularjack@gmail.com>
2016-10-29 18:53:20 +0100

    Add responsive behaviour

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wkhtmltopdf --print-media-type --page-size Letter -L 14mm -T 12mm -B 0mm -R 10mm index.html out.pdf
+wkhtmltopdf --print-media-type --page-size A4 -L 14mm -T 12mm -B 0mm -R 10mm index.html out.pdf
