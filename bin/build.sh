fda5e40c075931238cb7c61dd036a3204bff5322
Paulo Rodrigues Pinto <regularjack@gmail.com>
2016-10-29 17:20:02 +0100

    Port old version

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wkhtmltopdf --page-size Letter index.html out.pdf
+wkhtmltopdf --page-size Letter -L 14mm -T 12mm -B 0mm -R 10mm index.html out.pdf
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,10 +6,12 @@ rm -rf gh-pages
 git clone git@github.com:regularjack/cv.git --branch gh-pages --single-branch gh-pages
 
 cd gh-pages
+rm -rf *
 cp ../index.html .
 cp ../style.css .
+cp ../charissilr-webfont-en-subset.ttf .
 cp ../out.pdf .
 
 git add .
-git commit -m "Sync"
+git commit -am "Sync"
 git push origin gh-pages
