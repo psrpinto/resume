8211153996b3af66189ca35936ef68f431575baf
Paulo Rodrigues Pinto <regularjack@gmail.com>
2016-10-31 18:22:13 +0000

    Simplify watch script

--- bin/reload-browser.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/osascript -e "tell application \"Google Chrome\" to reload active tab of window 1"
--- bin/watch.sh
+++ bin/watch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ls index.html style.css | entr sh -c "bin/build.sh && bin/reload-browser.sh"
+ls index.html style.css | entr sh -c 'bin/build.sh && /usr/bin/osascript -e "tell application \"Google Chrome\" to reload active tab of window 1"'
